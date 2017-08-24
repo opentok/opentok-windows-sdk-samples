01847247037aa4bb80a74ba5e0d7df93321bcff1
Jeff Swartz <swartz@tokbox.com>
2017-08-24 14:31:37 -0700

    Minor docs edits

--- CustomVideoRenderer/VideoRenderer.cs
+++ CustomVideoRenderer/VideoRenderer.cs
@@ -26,7 +26,6 @@ namespace CustomVideoRenderer
     ///
     ///     xmlns:MyNamespace="clr-namespace:CustomRendererSample"
     ///
-    ///
     /// Step 1b) Using this custom control in a XAML file that exists in a different project.
     /// Add this XmlNamespace attribute to the root element of the markup file where it is 
     /// to be used:
@@ -36,9 +35,8 @@ namespace CustomVideoRenderer
     /// You will also need to add a project reference from the project where the XAML file lives
     /// to this project and Rebuild to avoid compilation errors:
     ///
-    ///     Right click on the target project in the Solution Explorer and
+    ///     Right-click on the target project in the Solution Explorer and
     ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
     ///
     /// Step 2)
     /// Go ahead and use your control in the XAML file.
--- SimpleMultiparty/README.md
+++ SimpleMultiparty/README.md
@@ -1,9 +1,9 @@
 SimpleMultiParty
 ================
 
-The SimpleMultiParty is a very simple application meant to get a new developer
-started using the OpenTok Windows SDK. For a full description, see the [Basic tutorial at the
-OpenTok developer center](https://tokbox.com/developer/tutorials/windows/basic-video-chat/).
+SimpleMultiParty is a simple application meant to get a new developer started using the
+OpenTok Windows SDK. For a full description, see the [Basic tutorial at the OpenTok developer
+center](https://tokbox.com/developer/tutorials/windows/basic-video-chat/).
 
 *Important:* To use this application, follow the instructions in the
 [Quick Start](../README.md#quick-start) section of the main README file
