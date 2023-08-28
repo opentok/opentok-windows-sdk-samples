9906b298b0de7d2ed466b5e229126e51fc06bbc0
Sergio Tur Gil <98801875+sergioturgil-vonage@users.noreply.github.com>
2023-08-28 08:19:36 +0200

    Updated AudioDevice notifications sample to 2.25.3 (#59)

---
+++ AudioDeviceNotifications/AudioInputControl.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using OpenTok;
+
+namespace AudioDeviceNotifications
+{
+    public partial class AudioInputControl : UserControl
+    {
+        #region Events
+
+        public delegate void AudioInputEnabledChangedHandler(bool isEnabled);
+
+        public event AudioInputEnabledChangedHandler AudioInputEnabledChanged;
+
+        #endregion
+
+        #region Constructors
+
+        public AudioInputControl()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Public
+
+        public void Init(Window parent, MMAudioDevice audioDevice)
+        {
+            this.parent = parent;
+            this.audioDevice = audioDevice;
+
+            ReloadAudioInputComboBox();
+
+            audioDevice.InputDeviceAdded += AudioInputDeviceAdded;
+            audioDevice.InputDeviceRemoved += AudioInputDeviceRemoved;
+            audioDevice.DefaultInputDeviceChanged += AudioDefaultInputDeviceChanged;
+        }
+
+        public void Close()
+        {
+            audioDevice.InputDeviceAdded -= AudioInputDeviceAdded;
+            audioDevice.InputDeviceRemoved -= AudioInputDeviceRemoved;
+            audioDevice.DefaultInputDeviceChanged -= AudioDefaultInputDeviceChanged;
+        }
+
+        #endregion
+
+        #region Private
+
+        private void AudioInputCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (AudioInputComboBox != null)
+            {
+                AudioInputComboBox.IsEnabled = true;
+                OnAudioInputEnabledChanged();
+            }
+        }
+
+        private void AudioInputCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (AudioInputComboBox != null)
+            {
+                AudioInputComboBox.IsEnabled = false;
+                OnAudioInputEnabledChanged();
+            }
+        }
+
+        private void AudioInputComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (AudioInputComboBox.SelectedItem != null)
+            {
+                AudioDevice.InputAudioDevice device = (AudioDevice.InputAudioDevice)AudioInputComboBox.SelectedItem;
+                audioDevice.SetInputAudioDevice(device.Id);
+            }
+        }
+
+        private void AudioInputDeviceAdded(object sender, AudioDevice.Notifications.InputAudioDeviceEventArgs e)
+        {
+            ReloadAudioInputComboBox();
+
+            _ = Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                MessageBoxResult messageBoxResult = MessageBox.Show(parent, string.Format("A new audio input device was detected: {0}\nWould you like to select this new device?", e.Device.Name), "New audio input device", MessageBoxButton.YesNo);
+                if (messageBoxResult == MessageBoxResult.Yes)
+                {
+                    AudioInputComboBox.SelectedItem = e.Device;
+                }
+            }));
+        }
+
+        private void AudioInputDeviceRemoved(object sender, AudioDevice.Notifications.InputAudioDeviceEventArgs e)
+        {
+            ReloadAudioInputComboBox();
+        }
+
+        private void AudioDefaultInputDeviceChanged(object sender, AudioDevice.Notifications.InputAudioDeviceEventArgs e)
+        {
+            _ = Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                AudioInputComboBox.SelectedItem = e.Device;
+            }));
+        }
+
+        private void ReloadAudioInputComboBox()
+        {
+            IList<AudioDevice.InputAudioDevice> availableAudioInputs = audioDevice.EnumerateInputAudioDevices();
+
+            _ = Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (availableAudioInputs == null || availableAudioInputs.Count == 0)
+                {
+                    AudioInputComboBox.ItemsSource = null;
+                }
+                else
+                {
+                    AudioDevice.InputAudioDevice currentSelection = (AudioDevice.InputAudioDevice)AudioInputComboBox.SelectedItem;
+                    AudioInputComboBox.ItemsSource = availableAudioInputs;
+                    if (currentSelection != null)
+                        AudioInputComboBox.SelectedItem = currentSelection;
+                    if (AudioInputComboBox.SelectedItem == null)
+                        AudioInputComboBox.SelectedItem = audioDevice.GetDefaultInputAudioDevice();
+                    if (AudioInputComboBox.SelectedItem == null)
+                        AudioInputComboBox.SelectedIndex = 0;
+                }
+                OnAudioInputEnabledChanged();
+            }));
+        }
+
+        private void OnAudioInputEnabledChanged()
+        {
+            bool isAudioInputEnabled = AudioInputCheckBox.IsChecked.HasValue && AudioInputCheckBox.IsChecked.Value && AudioInputComboBox.SelectedItem != null;
+            AudioInputEnabledChanged?.Invoke(isAudioInputEnabled);
+        }
+
+        private Window parent;
+        private MMAudioDevice audioDevice;
+
+        #endregion
+    }
+}
--- AudioDeviceNotifications/README.md
+++ AudioDeviceNotifications/README.md
@@ -1,8 +1,12 @@
 AudioDeviceNotifications
 ==============
 
-AudioDeviceNotifications is a simple application meant to showcase audio input and output change detection using the
-OpenTok Windows SDK.
+AudioDeviceNotifications improves on BasicVideoChat sample by adding an advanced audio management system with the following features:
+- Integration with default device selection at windows OS level.
+- Audio input/output device notifications that are invoked when devices are added/removed or the default selection changes.
+
+Note that removed devices are automatically detected, but added device take a few seconds to be detected by the system.
+In the default sample changing the default device at windows OS level also updates device selection in the app without user interaction (this behaviour can be easily changed).
 
 *Important:* To use this application, follow the instructions in the
 [Quick Start](../README.md#quick-start) section of the main README file for this repository.
