21075a13b4cbc9d54a603b1fc8b4df45d80a4013
sergioturgil-vonage <sergio.turgil@vonage.com>
2024-07-05 11:58:17 +0200

    Removed some freaking conditions that VS adds and cause problems when adding the targets for the native libraries. The problem is not present in projects using the current package import system (instead of the legacy packages.info) so I'll probably migrate projects at some point

--- BasicVideoChat/MainWindow.xaml.cs
+++ BasicVideoChat/MainWindow.xaml.cs
@@ -8,9 +8,9 @@ namespace BasicVideoChat
 {
     public partial class MainWindow : Window
     {
-        private const string API_KEY = "";
-        private const string SESSION_ID = "";
-        private const string TOKEN = "";
+        private const string API_KEY = "47797141";
+        private const string SESSION_ID = "2_MX40Nzc5NzE0MX5-MTcxOTkxMzg2NTc0OH52NWpQQzdNQUdQVEc4VDNhUmdlZ1loOXV-fn4";
+        private const string TOKEN = "T1==cGFydG5lcl9pZD00Nzc5NzE0MSZzZGtfdmVyc2lvbj0mc2lnPWQ2MDkzMmZlYzI4MDRlNTk1NmI4ZDgxZWUzNzNmM2E0MWUyMzJiOGE6c2Vzc2lvbl9pZD0yX01YNDBOemM1TnpFME1YNS1NVGN4T1RreE16ZzJOVGMwT0g1Mk5XcFFRemROUVVkUVZFYzRWRE5oVW1kbFoxbG9PWFYtZm40JmNyZWF0ZV90aW1lPTE3MTk5MTM4NjQmZXhwaXJlX3RpbWU9MTcyMjUwNTg2NCZyb2xlPW1vZGVyYXRvciZub25jZT0yYzk2ZDI5YS01ODQwLTQxZTQtYmY2YS00MjE1ZjIyZmQ2OGY=";
 
         private Context context;
         private Session Session;
@@ -34,11 +34,6 @@ namespace BasicVideoChat
                 Capturer = capturerDevices[0].CreateVideoCapturer(VideoCapturer.Resolution.High, VideoCapturer.FrameRate.Fps30),
                 Renderer = PublisherVideo
             }.Build();
-
-            IList<AudioDevice.InputAudioDevice> availableMics = AudioDevice.EnumerateInputAudioDevices();
-            if (availableMics == null || availableMics.Count == 0)
-                throw new Exception("No audio capture devices detected");
-            AudioDevice.SetInputAudioDevice(availableMics[0]);
 
             Session = new Session.Builder(context, API_KEY, SESSION_ID).Build();
             Session.Connected += Session_Connected;
