cb0d29e372b5d4c5322338e4254ce304483f3e48
sergioturgil-vonage <sergio.turgil@vonage.com>
2024-07-05 12:00:57 +0200

    Removed credentials

--- BasicVideoChat/MainWindow.xaml.cs
+++ BasicVideoChat/MainWindow.xaml.cs
@@ -8,9 +8,9 @@ namespace BasicVideoChat
 {
     public partial class MainWindow : Window
     {
-        private const string API_KEY = "47797141";
-        private const string SESSION_ID = "2_MX40Nzc5NzE0MX5-MTcxOTkxMzg2NTc0OH52NWpQQzdNQUdQVEc4VDNhUmdlZ1loOXV-fn4";
-        private const string TOKEN = "T1==cGFydG5lcl9pZD00Nzc5NzE0MSZzZGtfdmVyc2lvbj0mc2lnPWQ2MDkzMmZlYzI4MDRlNTk1NmI4ZDgxZWUzNzNmM2E0MWUyMzJiOGE6c2Vzc2lvbl9pZD0yX01YNDBOemM1TnpFME1YNS1NVGN4T1RreE16ZzJOVGMwT0g1Mk5XcFFRemROUVVkUVZFYzRWRE5oVW1kbFoxbG9PWFYtZm40JmNyZWF0ZV90aW1lPTE3MTk5MTM4NjQmZXhwaXJlX3RpbWU9MTcyMjUwNTg2NCZyb2xlPW1vZGVyYXRvciZub25jZT0yYzk2ZDI5YS01ODQwLTQxZTQtYmY2YS00MjE1ZjIyZmQ2OGY=";
+        private const string API_KEY = "";
+        private const string SESSION_ID = "";
+        private const string TOKEN = "";
 
         private Context context;
         private Session Session;
